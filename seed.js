a882d9cdeecb89e79b680f07bc7207e7459e914a
jpardike <jpardike@gmail.com>
2020-12-06 22:23:47 -0800

    Updated seed file

--- seed.js
+++ seed.js
@@ -2,11 +2,13 @@ const db = require("./models");
 const data = require("./userData.json");
 
 db.User.deleteMany({}, (err, deletedUser) => {
-  db.User.create(data.users, (err, seededUser) => {
-    if (err) console.log(err);
+  db.Post.deleteMany({}, (err, deletedPosts) => {
+    db.User.create(data.users, (err, seededUser) => {
+      if (err) console.log(err);
 
-    console.log(data.users.length, "users created successfully");
+      console.log(data.users.length, "users created successfully");
 
-    process.exit();
+      process.exit();
+    });
   });
 });
